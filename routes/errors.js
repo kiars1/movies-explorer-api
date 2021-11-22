dc36542e18918a37820f7cd6a3624f2a6c079e1f
kiars1 <deadhaze317@yandex.ru>
2021-11-22 16:33:34 +0700

    new proj

--- routes/errors.js
+++ routes/errors.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const NotFoundError = require('../errors/NotFoundError');
+const errorMesage = require('../utils/errorMesage');
 
 router.use('*', (req, res, next) => next(new NotFoundError(errorMesage.NotFoundPage)));
 
