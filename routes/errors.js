fff9e484c1197ee609c4de29a17dc06090426177
kiars1 <deadhaze317@yandex.ru>
2021-11-22 16:31:37 +0700

    new proj

--- routes/errors.js
+++ routes/errors.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const NotFoundError = require('../errors/NotFoundError');
 
-router.use('*', (req, res, next) => next(new NotFoundError('Запрашиваемый Ресурс не найден.')));
+router.use('*', (req, res, next) => next(new NotFoundError(errorMesage.NotFoundPage)));
 
 module.exports = router;
